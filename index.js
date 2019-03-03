635865a03e5d7f454235b78f9c71d3712a0315bb
Andrew Wang <andrewxiaoyu0@gmail.com>
2019-03-02 23:52:11 -0500

    Not real commit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const admin = require('firebase-admin')
 const express = require('express')
 const path = require('path')
-const moment = require('moment')
+// const moment = require('moment')
+const bodyParser = require('body-parser')
 
 /**
  * Firebase
@@ -12,7 +13,6 @@ admin.initializeApp({
 })
 
 const db = admin.firestore()
-const dataRef = db.collection('data')
 
 /**
  * Express
@@ -27,13 +27,16 @@ app.get('/', (req, res) => {
   res.sendFile('index.html')
 })
 
+app.use(bodyParser.json())
+
 /**
  * API
  */
 
 app.post('/api/readings/', (req, res) => {
-  const data = req.body.data
-  dataRef.doc(moment()).set(data).then(ref => {
+  console.log(req.body)
+  const data = req.body
+  db.collection('data').add(data).then(ref => {
     res.json({ success: true, ref: ref.id })
   }).catch(err => {
     res.json({ success: false, error: err })
