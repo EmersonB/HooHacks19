3314f64624bd14efe2df51600887e4a455741fe5
Kevin Guo <kev.guo123@gmail.com>
2019-03-02 20:13:21 -0600

    Update font

---
+++ public/js/scripts.js
@@ -0,0 +1,13 @@
+var config = {
+    apiKey: "AIzaSyAGR4ionKdXWPFxJQTUKxBHsJ-5RGiMtFU",
+    authDomain: "stasis-aff23.firebaseapp.com",
+    databaseURL: "https://stasis-aff23.firebaseio.com",
+    projectId: "stasis",
+    storageBucket: "stasis.appspot.com",
+    messagingSenderId: "911680701800"
+};
+firebase.initializeApp(config);
+
+$(document).ready(function() {
+  
+})
