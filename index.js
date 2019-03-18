e2d85737dfd26ee2ea8aeedcbb2b3e44d1b29b19
Matthew Crotty <mjc2011@verizon.net>
2019-03-18 13:06:49 -0400

    Added Favicon

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const path = require('path')
 const moment = require('moment')
 const bodyParser = require('body-parser')
-
+const favicon = require('express-favicon');
 /**
  * Firebase
  */
@@ -20,6 +20,8 @@ const db = admin.firestore()
 
 const app = express()
 const port = process.env.PORT || 8080
+
+app.use(favicon(__dirname + '/public/favicon.ico'));
 
 app.use(express.static(path.join(__dirname, '/public')))
 
